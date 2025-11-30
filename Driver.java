51a89603f519013703d668239af22c3748d14a73
Shevaunie Senior <shevjamsen123@gmail.com>
2025-11-29 20:18:22 -0500

    refreshed desktop

--- Driver.java
+++ Driver.java
@@ -1,13 +1,7 @@
 public class Driver {
-    static DataClass techData;
     static SystemUI GUI;
     public static void main(String[] args) {
-        techData = new DataClass(); 
         GUI = new SystemUI();
         GUI.setVisible(true);   
-    }
-
-    public static DataClass getData(){
-        return techData;
     }
 }
